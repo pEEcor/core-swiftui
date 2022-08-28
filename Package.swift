f0335aa673390809df558ce430dcbbc91dc97289
pEEcor <pEEcor@xcor.org>
2022-08-28 21:06:56 +0200

    Change target naming

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CoreSwiftUI",
-            targets: ["core-swiftui"]
+            targets: ["CoreSwiftUI"]
         ),
     ],
     dependencies: [
@@ -26,7 +26,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "core-swiftui",
+            name: "CoreSwiftUI",
             dependencies: []
         ),
 //        .testTarget(
